a227ec2ec6b010c55c193bc9276cc93ef18f01a3
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-09-12 17:57:50 +0530

    pkc%refac(bls): switch `Bls{Dh,Sk}Bytes` boilerplate to `make_bytes!`

--- pkgs/pkc/src/bls/dh_bytes.rs
+++ pkgs/pkc/src/bls/dh_bytes.rs
@@ -8,57 +8,12 @@
 
 use crate::bls::BlsSchemeId;
 
-use dash_types::derive_sbytes;
-use subtle::ConstantTimeEq;
-use zeroize::Zeroize;
-
-use core::marker::PhantomData;
+use dash_types::make_sbytes;
 
 /// Raw shared secret length (G1 compressed).
 pub const BLS_DH_LEN: usize = 48;
 
-/// A scheme-tagged Diffie-Hellman shared key, `sk * peer_pk`.
-pub struct BlsDhBytes<S: BlsSchemeId> {
-  inner: [u8; BLS_DH_LEN],
-  _scheme: PhantomData<S>,
-}
-
-impl<S: BlsSchemeId> BlsDhBytes<S> {
-  /// Wraps raw bytes.
-  pub const fn from_bytes(bytes: [u8; BLS_DH_LEN]) -> Self {
-    Self {
-      inner: bytes,
-      _scheme: PhantomData,
-    }
-  }
-
-  /// Borrows the inner byte array.
-  pub const fn as_bytes(&self) -> &[u8; BLS_DH_LEN] {
-    &self.inner
-  }
-}
-
-impl<S: BlsSchemeId> Zeroize for BlsDhBytes<S> {
-  fn zeroize(&mut self) {
-    self.inner.zeroize();
-  }
-}
-
-derive_sbytes!(for[S: BlsSchemeId] BlsDhBytes<S>, BLS_DH_LEN);
-
-impl<S: BlsSchemeId> Clone for BlsDhBytes<S> {
-  fn clone(&self) -> Self {
-    Self {
-      inner: self.inner,
-      _scheme: PhantomData,
-    }
-  }
-}
-
-impl<S: BlsSchemeId> Eq for BlsDhBytes<S> {}
-
-impl<S: BlsSchemeId> PartialEq for BlsDhBytes<S> {
-  fn eq(&self, other: &Self) -> bool {
-    self.inner.ct_eq(&other.inner).into()
-  }
+make_sbytes! {
+  /// A scheme-tagged Diffie-Hellman shared key, `sk * peer_pk`.
+  for[S: BlsSchemeId] BlsDhBytes<S>, BLS_DH_LEN, nocodec
 }
--- pkgs/pkc/src/bls/secret_bytes.rs
+++ pkgs/pkc/src/bls/secret_bytes.rs
@@ -14,79 +14,21 @@ use bitcoin_hashes::sha256d::Hash as Sha256d;
 use dash_num::Hash256;
 #[cfg(feature = "codec")]
 use dash_types::codec::Hashable;
-use dash_types::derive_sbytes;
-#[cfg(feature = "codec")]
-use dash_types::impl_sbytes;
-#[cfg(feature = "codec")]
-use dash_types::type_id::TypeId;
-use subtle::ConstantTimeEq;
-use zeroize::{Zeroize, Zeroizing};
-
-use core::marker::PhantomData;
+use dash_types::make_sbytes;
 
 /// Raw BLS secret key length (scalar).
 pub const BLS_SK_LEN: usize = 32;
 
-/// Scheme-tagged BLS secret key bytes (32 bytes, zeroized on drop).
-#[cfg_attr(feature = "codec", derive(TypeId))]
-pub struct BlsSkBytes<S: BlsSchemeId> {
-  inner: [u8; BLS_SK_LEN],
-  _scheme: PhantomData<S>,
+make_sbytes! {
+  /// Scheme-tagged BLS secret key bytes (32 bytes, zeroized on drop).
+  for[S: BlsSchemeId] BlsSkBytes<S>, BLS_SK_LEN
 }
-
-#[cfg(feature = "codec")]
-impl_sbytes!(for[S: BlsSchemeId] BlsSkBytes<S>, BLS_SK_LEN);
 
 #[cfg(feature = "codec")]
 impl<S: BlsSchemeId> Hashable for BlsSkBytes<S> {
   type Hash = Hash256;
 
   fn hash(&self) -> Self::Hash {
-    Hash256::from_bytes(Sha256d::hash(&self.inner).to_byte_array())
-  }
-}
-
-impl<S: BlsSchemeId> BlsSkBytes<S> {
-  /// Wraps raw bytes.
-  pub const fn from_bytes(bytes: [u8; BLS_SK_LEN]) -> Self {
-    Self {
-      inner: bytes,
-      _scheme: PhantomData,
-    }
-  }
-
-  /// Borrows the inner byte array.
-  pub const fn as_bytes(&self) -> &[u8; BLS_SK_LEN] {
-    &self.inner
-  }
-
-  /// Copies out the inner bytes in a zeroizing wrapper.
-  pub fn to_bytes(&self) -> Zeroizing<[u8; BLS_SK_LEN]> {
-    Zeroizing::new(self.inner)
-  }
-}
-
-impl<S: BlsSchemeId> Zeroize for BlsSkBytes<S> {
-  fn zeroize(&mut self) {
-    self.inner.zeroize();
-  }
-}
-
-derive_sbytes!(for[S: BlsSchemeId] BlsSkBytes<S>, BLS_SK_LEN);
-
-impl<S: BlsSchemeId> Clone for BlsSkBytes<S> {
-  fn clone(&self) -> Self {
-    Self {
-      inner: self.inner,
-      _scheme: PhantomData,
-    }
-  }
-}
-
-impl<S: BlsSchemeId> Eq for BlsSkBytes<S> {}
-
-impl<S: BlsSchemeId> PartialEq for BlsSkBytes<S> {
-  fn eq(&self, other: &Self) -> bool {
-    self.inner.ct_eq(&other.inner).into()
+    Hash256::from_bytes(Sha256d::hash(self.as_bytes()).to_byte_array())
   }
 }
