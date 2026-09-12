8d6f943e3282be5e641a3b82256b544a4fba3a97
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-09-12 17:56:34 +0530

    pkc%refac(bls): switch `Bls{Pk,Sig}Bytes` boilerplate to `make_bytes!`

--- pkgs/pkc/src/bls/public_bytes.rs
+++ pkgs/pkc/src/bls/public_bytes.rs
@@ -14,54 +14,21 @@ use bitcoin_hashes::sha256d::Hash as Sha256d;
 use dash_num::Hash256;
 #[cfg(feature = "codec")]
 use dash_types::codec::Hashable;
-use dash_types::derive_bytes;
-#[cfg(feature = "codec")]
-use dash_types::impl_bytes;
-#[cfg(feature = "codec")]
-use dash_types::type_id::TypeId;
-
-use core::marker::PhantomData;
+use dash_types::make_bytes;
 
 /// Raw BLS public key length (G1 compressed).
 pub const BLS_PK_LEN: usize = 48;
 
-/// Scheme-tagged BLS public key bytes (48 bytes, unvalidated).
-#[cfg_attr(feature = "codec", derive(TypeId))]
-pub struct BlsPkBytes<S: BlsSchemeId> {
-  inner: [u8; BLS_PK_LEN],
-  _scheme: PhantomData<S>,
+make_bytes! {
+  /// Scheme-tagged BLS public key bytes (48 bytes, unvalidated).
+  for[S: BlsSchemeId] BlsPkBytes<S>, BLS_PK_LEN
 }
-
-#[cfg(feature = "codec")]
-impl_bytes!(for[S: BlsSchemeId] BlsPkBytes<S>, BLS_PK_LEN);
 
 #[cfg(feature = "codec")]
 impl<S: BlsSchemeId> Hashable for BlsPkBytes<S> {
   type Hash = Hash256;
 
   fn hash(&self) -> Self::Hash {
-    Hash256::from_bytes(Sha256d::hash(&self.inner).to_byte_array())
+    Hash256::from_bytes(Sha256d::hash(self.as_bytes()).to_byte_array())
   }
 }
-
-impl<S: BlsSchemeId> BlsPkBytes<S> {
-  /// Wraps raw bytes.
-  pub const fn from_bytes(bytes: [u8; BLS_PK_LEN]) -> Self {
-    Self {
-      inner: bytes,
-      _scheme: PhantomData,
-    }
-  }
-
-  /// Borrows the inner byte array.
-  pub const fn as_bytes(&self) -> &[u8; BLS_PK_LEN] {
-    &self.inner
-  }
-
-  /// Returns the inner byte array.
-  pub const fn into_bytes(self) -> [u8; BLS_PK_LEN] {
-    self.inner
-  }
-}
-
-derive_bytes!(for[S: BlsSchemeId] BlsPkBytes<S>, BLS_PK_LEN);
